ecf32831491f4d1a27be14f2da7c57f228fd9a38
Arnoldas Kurbanovas <akurbanovas@albany.edu>
2016-10-21 17:21:56 -0400

    asrc meeting 10/21/2016

--- csvToNcUpdate.py
+++ csvToNcUpdate.py
@@ -52,12 +52,22 @@ lons_out = -125.0 + 5.0*arange(v5,dtype='float32')
 
 az = numpy.arange(-180,180,2.5)
 el = numpy.arange(-180,180,2.5)
-ranges = numpy.arange(100,10000,25)
+#ranges = numpy.arange(100,3000, 25)
 x = numpy.arange(-100,100,2.5)
 y = numpy.arange(-100,100,2.5)
 z = numpy.arange(-100,100,2.5)
 cnrs = numpy.arange(-100,100,2.5)
 conf = numpy.arange(0,100,100)
+
+#test
+#range_out = arange(v4,dtype='i8')
+rootgrp.createDimension('Range',None)
+Range = rootgrp.createVariable('Range',dtype('float32').char,('Range',))
+Range.units = 'm'
+ranges = numpy.arange(100,3000, )
+Range[:] = ranges
+Range = v4
+
 
 '''
 #Ranges/ output data
@@ -79,7 +89,7 @@ ncout.createDimension('longitude',v5)
 
 rootgrp.createDimension('Azimuth',None)
 rootgrp.createDimension('Elevation',None)
-rootgrp.createDimension('Range',None)
+
 rootgrp.createDimension('xWind',None)
 rootgrp.createDimension('yWind',None)
 rootgrp.createDimension('zWind',None)
@@ -101,7 +111,7 @@ lons = ncout.createVariable('longitude',dtype('float32').char,('longitude',))
 
 Azimuth = rootgrp.createVariable('Azimuth',dtype('float32').char,('Azimuth',))
 Elevation = rootgrp.createVariable('Elevation',dtype('float32').char,('Elevation',))
-Range = rootgrp.createVariable('Range',dtype('float32').char,('Range',))
+
 xWind = rootgrp.createVariable('xWind',dtype('float32').char,('xWind',))
 yWind = rootgrp.createVariable('yWind',dtype('float32').char,('yWind',))
 zWind = rootgrp.createVariable('zWind',dtype('float32').char,('zWind',))
@@ -111,7 +121,8 @@ ConfidenceIndex = rootgrp.createVariable('ConfidenceIndex',dtype('float32').char
 # Assign units attributes to coordinate var data. This attaches a text attribute to each of the coordinate variables,
 #  containing the units.
 Azimuth.units = "degrees"
-Range.units = 'm'
+Elevation.units = "degrees"
+
 xWind.units = 'm/s'
 yWind.units = 'm/s'
 zWind.units = 'm/s'
@@ -139,7 +150,7 @@ print rootgrp.__dict__
 # write data to coordinate vars.
 Azimuth[:] = az
 Elevation[:] = el
-Range[:] = ranges
+
 xWind[:] = x
 yWind[:] = y
 zWind[:] = z
@@ -149,6 +160,7 @@ ConfidenceIndex[:] = conf
 
 print "\n\n\n\n\n\n\n\n\nREPRINGING CHECK \n\n\n\n\n\n\n\n\n"
 
+#print Range
 
 #reprinting check:
 print rootgrp.data_model
@@ -170,114 +182,7 @@ print rootgrp.__dict__
 rootgrp.close()
 #f.close()
 
-'''
-#To create an unlimited dimension (a dimension that can be appended to), the size value is set to None or 0
-#name_str = rootgrp.createDimension('name_str', 50)
 
-
-reconMeasure = rootgrp.createDimension('reconMeasure', None)
-TimeStamp = rootgrp.createDimension('TimeStamp', None)
-
-#printing the dimensions from python dictionary
-print rootgrp.dimensions
-
-#printing the name and length of the dimensions and showing what is unlimited
-for dimobj in rootgrp.dimensions.values():
-  print dimobj
-
-# create the variables
-TimeStamp = rootgrp.createVariable("TimeStamp","f8",("TimeStamp",))
-TimeStamp.standard_name = 'TimeStamp'
-TimeStamp.long_name = 'Time of measurement'
-TimeStamp.units = 'seconds since 1970-01-01 00:00:00'
-
-Azimuth = rootgrp.createVariable("Azimuth", "i8" , ("reconMeasure",))
-Azimuth.standard_name = 'Azimuth'
-Azimuth.units = "degrees"
-
-Elevation = rootgrp.createVariable("Elevation", "f8", ("reconMeasure",))
-Elevation.standard_name = 'Elevation'
-Elevation.units = "degrees"
-
-Range = rootgrp.createVariable("Range", "f4", ("reconMeasure",))
-Range.standard_name = 'Range'
-Range.units = 'm'
-
-xWind = rootgrp.createVariable("xWind", "i8", ("reconMeasure",))
-xWind.standard_name = 'X-Wind Speed'
-xWind.units = 'm/s'
-
-yWind = rootgrp.createVariable("yWind", "i8", ("reconMeasure",))
-yWind.standard_name = 'Y-Wind Speed'
-yWind.units = 'm/s'
-
-zWind = rootgrp.createVariable("zWind", "i8", ("reconMeasure",))
-zWind.standard_name = 'Z-Wind Speed'
-zWind.units = 'm/s'
-
-CNR = rootgrp.createVariable("CNR", "i8", ("reconMeasure",))
-CNR.standard_name = 'CNR'
-CNR.units = 'db'
-
-ConfidenceIndex = rootgrp.createVariable("ConfidenceIndex", "u1", ("reconMeasure",))
-ConfidenceIndex.standard_name = 'Confidence index'
-ConfidenceIndex.units = '%'
-
-# printing python dictionary with all the current variables
-print rootgrp.variables
-
-# set the global attributes
-import time
-rootgrp.description = "lidar data csv to netCDF script"
-rootgrp.history = "Created " + time.ctime(time.time())
-rootgrp.source = "netCDF4 python module"
-
-#units specified above
-
-latitudes.units = "degrees north"
-longitudes.units = "degrees east"
-levels.units = "hPa"
-temp.units = "K"
-times.units = "hours since 0001-01-01 00:00:00.0"
-times.calendar = "gregorian"
-
-
-for name in rootgrp.ncattrs():
-    print "Global attr", name, "=", getattr(rootgrp,name)
-
-#providing all the netCDF attribute name/value pairs in a python dictionary
-print rootgrp.__dict__
-
-import numpy
-
-lats =  numpy.arange(-90,91,2.5)
-lons =  numpy.arange(-180,180,2.5)
-latitudes[:] = lats
-longitudes[:] = lons
-print "latitudes =\n",latitudes[:]
-
-
-az = numpy.arange(-180,180,2.5)
-el = numpy.arange(-180,180,2.5)
-ranges = numpy.arange(100,10000,25)
-x = numpy.arange(-100,100,2.5)
-y = numpy.arange(-100,100,2.5)
-z = numpy.arange(-100,100,2.5)
-cnrs = numpy.arange(-100,100,2.5)
-conf = numpy.arange(0,100,100)
-
-Azimuth[:] = az
-Elevation[:] = el
-Range[:] = ranges
-xWind[:] = x
-yWind[:] = y
-zWind[:] = z
-CNR[:] = cnrs
-ConfidenceIndex[:] = conf
-
-print "Range =\n", Range[:]
-
-'''
 
 
 

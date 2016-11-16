8c1c8b21cd668d28f0f3847c342e1d9a98846fc7
Arnoldas Kurbanovas <akurbanovas@albany.edu>
2016-11-15 22:32:26 -0500

    cleanNC added

--- cleanNC.py
+++ cleanNC.py
@@ -1,39 +1,12 @@
+#cleaned up version of newNC
 #everything seperate variables working + adding wind with everything + TimeStamps working
 #+ currently working on coordinate variables to work in wind and winds
-import csv
+
 import datetime, time
-import pandas
-import os, sys
-import netCDF4
-from stat import S_ISREG, ST_CTIME, ST_MODE
 import numpy
 import numpy as np
 import netCDF4
 
-
-#parsing an xml document
-'''
-EX:
-<data>
-    <items>
-        <item name="item1">item1</item>
-        <item name="item2">item2</item>
-        <item name="item3">item3</item>
-        <item name="item4">item4</item>
-    </items>
-</data>
-
-from xml.dom import minidom
-xmldoc = minidom.parse('items.xml')
-itemlist = xmldoc.getElementsByTagName('item')
-print "Len : ", len(itemlist)
-print "Attribute Name : ", itemlist[0].attributes['name'].value
-print "Text : ", itemlist[0].firstChild.nodeValue
-for s in itemlist :
-    print "Attribute Name : ", s.attributes['name'].value
-    print "Text : ", s.firstChild.nodeValue
-
-'''
 #parsing xml document to get range steps
 from xml.dom import minidom
 xmldoc = minidom.parse('/Users/arnoldas/Desktop/Fall 2016/ASRC/sourcefolder/20161002_scan.xml')
@@ -42,17 +15,6 @@ itemlist = xmldoc.getElementsByTagName('scan')
 print "Attribute Name : ", itemlist[0].attributes['display_resolution_m'].value
 rangeXML = itemlist[0].attributes['display_resolution_m'].value
 print int(rangeXML)
-
-'''
-from lxml import etree
-doc = etree.parse(filename)
-
-memoryElem = doc.find('memory')
-print memoryElem.text        # element text
-print memoryElem.get('unit') # attribute
-'''
-
-from numpy import arange, dtype
 
 # lat/lon of random location for testing
 station_lat   = 50.317993
@@ -72,14 +34,6 @@ v9 = [] #Confidence
 f = open('/Users/arnoldas/Desktop/Fall 2016/ASRC/sourcefolder/20161002_reconstruction_wind_data.csv', 'r').readlines()
 #f = open('/Users/arnoldas/Desktop/Fall 2016/ASRC/sourcefolder/20160809_whole_radial_wind_data.csv', 'r').readlines()
 #Index of /private/enhanced/lidar_raw/CESTM_roof-76/2016/10
-
-'''
-with open("myfile.csv") as infile:
-    for line in infile:
-        appendtoNetcdf(line)
-'''
-
-
 
 epoch = datetime.datetime.utcfromtimestamp(0)
 
@@ -116,22 +70,12 @@ for line in f[1:]:
 #print v1
 
 from netCDF4 import Dataset
-#rootgrp = Dataset("test.nc", "w", format="NETCDF4")
-#rootgrp = netCDF4.Dataset('station_data.nc','w')
-#convert to netcdf4 framework that works as a netcdf
+
 rootgrp = Dataset('/Users/arnoldas/Desktop/Fall 2016/ASRC/targetfolder/reconData.nc', "w", format="NETCDF4")
 print rootgrp.data_model
-#NETCDF4
-#to close netCDF file
-#rootgrp.close()
+
 
 epoch = datetime.datetime.utcfromtimestamp(0)
-
-# create the dimensions.
-'''
-ncout.createDimension('latitude',v4)
-ncout.createDimension('longitude',v5)
-'''
 
 #dimensions
 TimeStamp = rootgrp.createDimension('TimeStamp', None)
@@ -146,7 +90,6 @@ print rootgrp.dimensions
 #printing the name and length of the dimensions and showing what is unlimited
 for dimobj in rootgrp.dimensions.values():
   print dimobj
-
 
 
 # create the variables
@@ -211,20 +154,6 @@ rootgrp.history = "Created " + time.ctime(time.time())
 rootgrp.source = "netCDF4 python module"
 
 
-
-az = numpy.arange(-180,180,2.5)
-el = numpy.arange(-180,180,2.5)
-
-x = numpy.arange(-100,100,2.5)
-y = numpy.arange(-100,100,2.5)
-z = numpy.arange(-100,100,2.5)
-cnrs = numpy.arange(-100,100,2.5)
-conf = numpy.arange(0,100,100)
-
-
-
-
-
 #adding data to lats and longs
 lats =  numpy.arange(-90,91,2.5)
 lons =  numpy.arange(-180,180,2.5)
@@ -260,44 +189,22 @@ my_data = np.reshape(var,(ny,nx),'F')
 
 #assignming data to everything
 Time[:] = timestamp
-#Azimuth[:] = v2
-#Elevation[:] = v3
-#Range[:] = v4
-#xWind[:] = timestamp,v4,lats,lons
-#yWind[:] = v6
-#zWind[:] = v7
-#CNR[:] = v8
-#ConfidenceIndex[:] = v9
-#wind[:,:,:,:,:,:] = v2,v3,v4,v5,v6,v7
 
 # appending along two unlimited dimensions by assigning to slice
 nlats = len(rootgrp.dimensions["lat"])
 nlons = len(rootgrp.dimensions["lon"])
 nRange = len(rootgrp.dimensions["Range"])
 ntime = len(timestamp)
-#nAzimuth = len(rootgrp.dimensions["Azimuth"])
-#nElevation = len(rootgrp.dimensions["Elevation"])
 
-#nxWind = len(rootgrp.dimensions["xWind"])
-#nyWind = len(rootgrp.dimensions["yWind"])
-#nzWind = len(rootgrp.dimensions["zWind"])
 
 
 print ntime
 print nRange
 
-#print "windx shape before adding data = ",xWind.shape
-#print "wind shape before adding data = ",wind.shape
-#wind[:,:,:,:,:,:] = []
-
-from numpy.random import uniform
-#wind[:] = np.asarray(v2)
 from numpy.random import uniform
 
-#winds[0:5,0:10,0:10,0:10,:,:] = uniform(size=(5,10,10,10,nlats,nlons))
-#wind[0:359294,0:359294] = uniform(size=(359294,359294))
-#winds[:,:,:,:,:,:] = uniform(size=(ntime,ntime,ntime,ntime,nlats,nlons))
 Wind[0:5,:,:] = uniform(size=(5,nlats,nlons))
+
 print "windx shape after adding data = ",Wind.shape
 #print "wind shape before adding data = ",wind.shape
 #np.asarray(v)
@@ -350,24 +257,6 @@ data[:] = array
 
 print "wind shape after adding data = ",Wind.shape
 
-'''
-# append along two unlimited dimensions by assigning to slice.
->>> nlats = len(rootgrp.dimensions["lat"])
->>> nlons = len(rootgrp.dimensions["lon"])
->>> print "temp shape before adding data = ",temp.shape
-temp shape before adding data =  (0, 0, 73, 144)
->>>
->>> from numpy.random import uniform
->>> temp[0:5,0:10,:,:] = uniform(size=(5,10,nlats,nlons))
->>> print "temp shape after adding data = ",temp.shape
-temp shape after adding data =  (6, 10, 73, 144)
-'''
-
-from numpy.random import uniform
-#temp[0:5,0:10,:,:] = uniform(size=(5,10,nlats,nlons))
-#wind[:] = (v2,v3)
-#wind[:,:] = (Azimuth,Elevation)
-print ntime
 print "****************\n\n\n\n\n\n\n********************"
 
 
